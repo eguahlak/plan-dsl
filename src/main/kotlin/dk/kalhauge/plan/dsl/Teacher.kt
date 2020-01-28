a00fa5ff5f36fce76d50e83e8859dc0bab546705
eguahlak <anders@kalhauge.dk>
2020-01-28 01:17:43 +0100

    using document-dsl:1.3.1

--- src/main/kotlin/dk/kalhauge/plan/dsl/Teacher.kt
+++ src/main/kotlin/dk/kalhauge/plan/dsl/Teacher.kt
@@ -2,7 +2,4 @@ package dk.kalhauge.plan.dsl
 
 class Teacher(val initials: String, val name: String, val phone: String?)
 
-fun Lecture.teachers(vararg teachers: Teacher) {
-  teachers.forEach { add(it) }
-  }
 
