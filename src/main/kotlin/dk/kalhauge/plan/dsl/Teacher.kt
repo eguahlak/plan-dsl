fefd5b6afc132467600537b22e6805e2bcce6ae3
eguahlak <anders@kalhauge.dk>
2021-01-15 17:49:09 +0100

    version 2.3.2 toString() implemented on Teacher

--- src/main/kotlin/dk/kalhauge/plan/dsl/Teacher.kt
+++ src/main/kotlin/dk/kalhauge/plan/dsl/Teacher.kt
@@ -1,5 +1,7 @@
 package dk.kalhauge.plan.dsl
 
-class Teacher(val initials: String, val name: String, val phone: String?)
+class Teacher(val initials: String, val name: String, val phone: String?) {
+  override fun toString() = "$name ($initials)"
+  }
 
 
